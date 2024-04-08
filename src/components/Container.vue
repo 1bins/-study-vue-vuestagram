af7c732e5f2dde522fc1a52baf0a49053aeaebd8
Wonbin Seo <1bins@kakao.com>
2024-04-08 22:08:17 +0900

    서버로 ajax 요청하는 더보기 버튼 만들기

---
+++ src/components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Post
+      v-for="(item, i) in instaData"
+      :key="`instaData-${i}`"
+      :instaData="item"
+    />
+  </div>
+</template>
+
+<script>
+  import Post from '@/components/Post.vue'
+  export default {
+    name: 'Container',
+    components: {
+      Post
+    },
+    props: {
+      instaData: Array,
+    }
+  }
+</script>
+
+<style>
+
+</style>
